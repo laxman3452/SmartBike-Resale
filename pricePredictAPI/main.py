361ff2e9f73119e2fdb2dc8ac853602e70f30cbc
Laxman Poudel <contactlaxman99@gmail.com>
2025-05-11 10:16:12 +0545

    Update main.py

--- pricePredictAPI/main.py
+++ pricePredictAPI/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
 from schemas import BikeDetails
-from model import predict_price
+from typing import Any
+import numpy as np
+import pickle
+
+# Load model once when app starts
+with open("model/used_bike_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Bike Price Prediction API"}
+# Define schema (you can also import this from schemas.py)
 
-@app.post("/predict/")
-def predict(data: BikeFeatures):
-    prediction = predict_price(data.dict())
-    return {"predicted_price": prediction}
+@app.post("/predict")
+def predict_price(data: BikeDetails) -> Any:
+    input_features = np.array([[data.brand,
+                                 data.bike_name,
+                                 data.year_of_purchase,
+                                 data.cc,
+                                 data.kms_driven,
+                                 data.owner,
+                                 data.servicing,
+                                 data.engine_condition,
+                                 data.physical_condition,
+                                 data.tyre_condition]],
+                               dtype=object)
+
+    prediction = model.predict(input_features)
+    return {"predicted_price": round(prediction[0], 2)}
